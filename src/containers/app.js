a77c83af45c81823cbb27ca1f4b6c3a4521a30d4
hodari00 <omar.albaiti@yahoo.com>
2016-08-11 20:15:18 +0200

    Add files via upload

---
+++ src/containers/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import UsersBlock from '../components/UsersBlock';
+import ChatInput from '../components/ChatInput';
+import ChatHistory from '../components/ChatHistory';
+
+class App extends React.Component {
+	render(){
+		const { sendMessage, state } = this;
+	  	return (
+	    	<div>
+	    		<UsersBlock userID={ state.userID } />
+	    		<ChatHistory history={ state.history } />
+	    		<ChatInput userID={ state.userID } sendMessage={ sendMessage } />
+	    	</div>
+	  	);
+	}
+
+	state = {
+    userID: Math.round(Math.random() * 1000000),
+    history: [],
+  };
+  sendMessage = (message) => this.setState({ 
+      history: this.state.history.concat(message) 
+    })
+}
+
+export default App;
